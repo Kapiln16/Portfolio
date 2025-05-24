f55567b87a0bf6f2c303a7090622026bdbbf7df5
Kapil Neela <neelakapil15@gmail.com>
2025-05-24 15:16:54 +0530

    index.js

--- BMIcalc/index.js
+++ BMIcalc/index.js
@@ -9,24 +9,40 @@ modebtn.addEventListener("click", () =>{
         document.querySelector("#mode").style.backgroundColor = "gray";
         document.querySelector(".main").style.backgroundColor = "beige";
         document.querySelector(".loadline").style.backgroundColor = "beige";
+        document.querySelector(".main").style.color = "gray";
+        document.querySelector("#submit").style.backgroundColor = "gray";
+        document.querySelector("#submit").style.color = "beige";
     }
     else{
         currmode = "light"
         document.querySelector("body").style.backgroundColor = "beige";
-        document.querySelector("header").style.color = "grey";
+        document.querySelector("header").style.color = "gray";
         document.querySelector("#mode").style.backgroundColor = "beige";
-        document.querySelector(".main").style.backgroundColor = "grey";
-        document.querySelector(".loadline").style.backgroundColor = "grey";
+        document.querySelector(".main").style.backgroundColor = "gray";
+        document.querySelector(".loadline").style.backgroundColor = "gray";
+        document.querySelector(".main").style.color = "beige";
+        document.querySelector("#submit").style.backgroundColor = "beige";
+        document.querySelector("#submit").style.color = "gray";
+
     }
 
 });
 
+
+
+
+
+
 function calculateBMI() {
   const weight = parseFloat(document.getElementById("weight").value);
   const height = parseFloat(document.getElementById("height").value);
+  //const height1 = parseFloat(document.getElementById("height1").value);
+  //const height2 = parseFloat(document.getElementById("height2").value);
+
+  //mainheight = (height1 + height2/12)*2.54;  
 
   if (weight > 0 && height > 0) {
-    const bmi = weight / (height * height);
+    const bmi = weight / ((height * height)/10000);
     document.getElementById("result").innerText = "Your BMI is: " + bmi.toFixed(2);
   } else {
     document.getElementById("result").innerText = "Please enter valid height and weight.";
